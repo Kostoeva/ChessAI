acde85c0b969102e646a27711775042ce86fd439
unknown <irmareka@gmail.com>
2018-01-19 23:15:05 -0800

    adding edge cases to Knight

--- src/com/chess/engine/pieces/Knight.java
+++ src/com/chess/engine/pieces/Knight.java
@@ -2,6 +2,7 @@ package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Tile;
 import com.google.common.collect.ImmutableList;
@@ -18,14 +19,21 @@ public class Knight extends Piece {
     }
 
     @Override
-    public List<Move> calculateLegalMoves(Board board) {
+    public Collection<Move> calculateLegalMoves(Board board) {
 
         int candidateDestinationCoordinate;
         List<Move> legalMoves = new ArrayList<>();
 
         for (final int currentCandidate : CANDIDATE_MOVE_COORDINATES) {
             candidateDestinationCoordinate = this.piecePosition + currentCandidate;
-            if (isValidTileCoordinate(candidateDestinationCoordinate)) {
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                if (isFirstColumnExclusion(this.piecePosition, currentCandidate) ||
+                        isSecondColumnExclusion(this.piecePosition, currentCandidate) ||
+                        isSeventhColumnExclusion(this.piecePosition, currentCandidate) ||
+                        isEighthColumnExclusion(this.piecePosition, currentCandidate)) {
+                    continue;
+                }
+
                 final Tile candidateDestinationTile =
                         board.getTile(candidateDestinationCoordinate);
 
@@ -43,12 +51,31 @@ public class Knight extends Piece {
             }
             return ImmutableList.copyOf(legalMoves);
         }
-
-
         return null;
     }
 
-    private boolean isValidTileCoordinate(int coordinate) {
-        return coordinate >= 0 && coordinate < 64;
+    private static boolean isFirstColumnExclusion(final int currentPos, final int candidatePos) {
+        return BoardUtils.FIRST_COLUMN[currentPos] &&
+                ((candidatePos == -17) ||
+                        (candidatePos == -10) ||
+                        (candidatePos == 6) ||
+                        (candidatePos == 15));
+    }
+
+    private static boolean isSecondColumnExclusion(final int currentPos, final int candidatePos) {
+        return BoardUtils.SECOND_COLUMN[currentPos] && ((candidatePos == -10) || (candidatePos == 6));
+    }
+
+    private static boolean isSeventhColumnExclusion(final int currentPos, final int candidatePos) {
+        return BoardUtils.SEVENTH_COLUMN[currentPos] && ((candidatePos == 10) || (candidatePos == -6));
+    }
+
+    private static boolean isEighthColumnExclusion(final int currentPos, final int candidatePos) {
+        return BoardUtils.EIGHTH_COLUMN[currentPos] &&
+                ((candidatePos == 17) ||
+                        (candidatePos == 10) ||
+                        (candidatePos == -6) ||
+                        (candidatePos == -15));
     }
 }
+
